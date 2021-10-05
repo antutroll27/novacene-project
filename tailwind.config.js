b90c232ddbfa17fdab0ce13d40533c027be37b3e
Antariksha Kumar <antarixmusic@gmail.com>
2021-10-05 23:49:34 +0530

    Installed DaisyUI
    DaisyUI is a Componenets library for Tailwind.css

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [
+    './src/**/*.html',
+    './src/**/*.js',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  fontFamily: {
+    body:['Poppins']
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+}
